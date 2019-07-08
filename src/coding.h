e31f30c3ab9fcee452d51a6576e73591ca5a6924
rexq57 <rexq57c@qq.com>
2019-07-09 03:16:23 +0800

    ok jsondecoder

--- src/coding.h
+++ src/coding.h
@@ -1,25 +1,17 @@
 #pragma once
 
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-
-#include <vector>
-#include <map>
-#include <string>
-
 namespace coding {
     
     class Coder {
     public:
-        template<typename T> inline
+        template<typename A, typename T> inline
         void encode(const T& value, const char* key);
     };
     
     
     class Decoder {
     public:
-        template<typename T> inline
+        template<typename A, typename T> inline
         T decode(const char* key);
     };
     
@@ -31,155 +23,30 @@ namespace coding {
         virtual bool initWithCoder(Decoder* decoder) = 0;
     };
     
-    using namespace rapidjson;
-    
-    
-    
-    class JsonCoder: public Coder {
-        
-    public:
-        JsonCoder() {
-            _doc.SetObject(); // 必须设置
-        };
-        
-        template<typename T, std::enable_if_t<!std::is_base_of<Codable, T>::value, int> = 0> inline
-        void encode(const T& value, const char* key){
-            _addValue(value, key);
-        };
-        
-        template<typename T, std::enable_if_t<std::is_base_of<Codable, T>::value, int> = 0> inline
-        void encode(const T& obj, const char* key)
-        {
-            JsonCoder coder;
-            obj.encodeWithCoder(&coder);
-            Document& doc = coder._doc;
-            
-            _doc.AddMember(Value().SetString(key, (unsigned int)strlen(key)), doc, _doc.GetAllocator());
-            
-            _rebuildBuffer = true;
-        }
-        
-        void encode(const std::string& str, const char* key)
-        {
-            _addValue(str.c_str(), key);
-        }
-        
-        const char* string()
-        {
-            rapidjson::StringBuffer& buffer = _buffer;
-            if (_rebuildBuffer)
-            {
-                buffer.Clear();
-                rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-                _doc.Accept(writer);
-                _rebuildBuffer = false;
-            }
-            
-            
-            return buffer.GetString();
-        }
-        
-    private:
-        
-        template<typename T>
-        void _addValue(const T& value, const char* key)
-        {
-            _getValue(key).Set(value);
-            _rebuildBuffer = true;
-        }
-
-        Value& _getValue(const char* key) {
-            Document& d = _doc;
-            
-            if (d.HasMember(key)) assert(!"已经存在！会导致该键重复存在于json中！");
-            
-            Value v;
-            
-            d.AddMember(Value().SetString(key, (unsigned int)strlen(key)), v, d.GetAllocator());
-            
-            return d[key];
-        }
-        
-        bool _rebuildBuffer = true;
-        rapidjson::StringBuffer _buffer = 0;
-        rapidjson::Document _doc;
-    };
-    
-    class JsonDecoder: public Decoder {
-        
-    public:
-        
-        JsonDecoder(const char* json) : JsonDecoder(new Document()) {
-            _doc->Parse(json);
-        }
-        
-        virtual ~JsonDecoder()
-        {
-            if (!_assign && _doc)
-                delete _doc;
-        }
-        
-        template<typename T, std::enable_if_t<!std::is_base_of<Codable, T>::value, int> = 0> inline
-        T decode(const char* key)
-        {
-            return (*_doc)[key].Get<T>();
-        }
-        
-        const char* decode(const char* key)
-        {
-            return (*_doc)[key].GetString();
-        }
-        
-        
-        template<typename T, std::enable_if_t<std::is_base_of<Codable, T>::value, int> = 0> inline
-        std::shared_ptr<T> decode(const char* key)
-        {
-            T* obj = new T();
-            
-            Value& doc = (*_doc)[key];
-            JsonDecoder decoder((Document*)&doc, true);
-            
-            if (!obj->initWithCoder(&decoder)) return 0;
-            
-            return std::shared_ptr<T>(obj);
-        }
-        
-    private:
-        
-        JsonDecoder(Document* doc, bool assign=false)
-        {
-            _doc = doc;
-            _assign = assign;
-        }
-        
-        bool _assign = false;
-        Document* _doc = 0;
-    };
-    
-    template<typename T> inline
+    template<typename A, typename T> inline
     void encode(Coder* x, const T& value, const char* key)
     {
-        ((JsonCoder*)x)->encode(value, key);
+        ((A*)x)->encode(value, key);
     }
     
-    template<typename T> inline
+    template<typename A, typename T> inline
     T decode(Decoder* x, const char* key)
     {
-        return ((JsonDecoder*)x)->decode<T>(key);
+        return ((A*)x)->template decode<T>(key);
     }
     
     //////////////////////////////////////////////////////////////////////////
     // Coder转发实现
-    template<typename T> inline
+    template<typename A,typename T> inline
     void Coder::encode(const T& value, const char* key)
     {
-        coding::encode(this, value, key);
+        coding::encode<A>(this, value, key);
     }
     
-    template<typename T> inline
+    template<typename A, typename T> inline
     T Decoder::decode(const char* key)
     {
-        return coding::decode<T>(this, key);
+        return coding::decode<A, T>(this, key);
     }
     
     //////////////////////////////////////////////////////////////////////////
